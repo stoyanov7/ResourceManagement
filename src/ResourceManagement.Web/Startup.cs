4baee91753f9843c1a1955f592ebb0599df8fd49
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-09-30 13:45:32 +0300

    Add missing libman.json

--- src/ResourceManagement.Web/Startup.cs
+++ src/ResourceManagement.Web/Startup.cs
@@ -1,17 +1,20 @@
 namespace ResourceManagement.Web
 {
     using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Components.Authorization;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using ResourceManagement.Web.Areas.Identity;
+    using ResourceManagement.Web.Data;
     using ResourceManagement.Data;
     using ResourceManagement.Domain;
-    using ResourceManagement.Domain.Logger;
     using ResourceManagement.Infrastructure;
+    using ResourceManagement.Domain.Logger;
     using ResourceManagement.Infrastructure.Logger;
-    using ResourceManagement.Web.Data;
 
     public class Startup
     {
@@ -21,18 +24,21 @@ namespace ResourceManagement.Web
         }
 
         public IConfiguration Configuration { get; }
-
+       
         public void ConfigureServices(IServiceCollection services)
         {
-            services
-                .AddDbContext<ResourceManagementContext>(options => options.UseNpgsql(this.Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ResourceManagementContext>(options =>
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<ResourceManagementContext>();
 
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             services.AddSingleton<WeatherForecastService>();
-
-            services.AddScoped(typeof(IMyLogger<>), typeof(MyLogger<>));
             services.AddTransient<IScheduleService, ScheduleService>();
+            services.AddScoped(typeof(IMyLogger<>), typeof(MyLogger<>));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -40,10 +46,12 @@ namespace ResourceManagement.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
             }
             else
             {
                 app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -52,8 +60,12 @@ namespace ResourceManagement.Web
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
