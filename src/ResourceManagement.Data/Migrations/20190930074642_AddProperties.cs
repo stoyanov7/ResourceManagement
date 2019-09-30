9894de1ffd1b4fef430d5e1c7f6b7718292f5e4e
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-09-30 10:48:13 +0300

    Add ne properties to database tables

---
+++ src/ResourceManagement.Data/Migrations/20190930074642_AddProperties.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ResourceManagement.Data.Migrations
+{
+    public partial class AddProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "ScheduleItem",
+                nullable: true);
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "Duration",
+                table: "RecurringSchedule",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "RecurringSchedule",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "ScheduleItem");
+
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "RecurringSchedule");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "RecurringSchedule");
+        }
+    }
+}
